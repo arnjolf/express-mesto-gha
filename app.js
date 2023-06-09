8506a3046c17be6dee7de9fb409ab8d9565aa234
arnjolf <arnjolff@gmail.com>
2023-06-10 00:02:45 +0600

    helmet

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const router = require('./routes/index');
 
 const app = express();
 app.use(express.json());
+app.use(helmet());
 app.use((req, res, next) => {
   req.user = {
     _id: '647f14af82c06f5a0bf97911', // вставьте сюда _id созданного в предыдущем пункте пользователя
