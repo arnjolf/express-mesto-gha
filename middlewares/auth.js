535ce0be25d0e7194e046c2776f7117d1fe68c4c
arnjolf <arnjolff@gmail.com>
2023-06-21 07:09:51 +0600

    unauthorized fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-const UnauthorizedError = require('../errors/UnauthorizedError');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
