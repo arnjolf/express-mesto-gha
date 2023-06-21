a4c48a82a6afcdeee3c5be753b4faabcfa39a664
arnjolf <arnjolff@gmail.com>
2023-06-21 07:07:56 +0600

    unauthorized fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,14 +5,14 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return new UnauthorizedError('Необходима авторизация');
+    return res.status(401).send({ message: 'Необходима авторизация' });
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return new UnauthorizedError('Необходима авторизация');
+    return res.status(401).send({ message: 'Необходима авторизация' });
   }
   req.user = payload;
 
